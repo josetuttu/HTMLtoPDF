23135f57d36e06456901859b44b411ab4a653bf9
Jose Tuttu George <josetuttu@gmail.com>
2016-10-21 11:00:24 +0530

    Added initial draft solution

---
+++ HTMLtoPDF/HTMLtoPDF/PdfProcessor.cs
@@ -0,0 +1,73 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLtoPDF
+{
+    class PdfProcessor
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllBytes(@"D:\temp\pdf1.pdf", GeneratePDF("", PageSize.A4));
+        }
+
+        public static byte[] GeneratePDF(string html, Rectangle pageSize) {
+            //Create a byte array that will eventually hold our final PDF
+            Byte[] bytes;
+
+            //Boilerplate iTextSharp setup here
+            //Create a stream that we can write to, in this case a MemoryStream
+            using (var ms = new MemoryStream())
+            {
+
+                //Create an iTextSharp Document which is an abstraction of a PDF but **NOT** a PDF
+                using (var doc = new Document())
+                {
+
+                    //Create a writer that's bound to our PDF abstraction and our stream
+                    using (var writer = PdfWriter.GetInstance(doc, ms))
+                    {
+
+                        //Open the document for writing
+                        doc.Open();
+
+                        //Our sample HTML and CSS
+                        var example_html = File.ReadAllText(@"D:\temp\template.html");
+                        //var example_css = @".headline{font-size:200%}";
+
+                        
+                        /**************************************************
+                         * Example #2                                     *
+                         *                                                *
+                         * Use the XMLWorker to parse the HTML.           *
+                         * Only inline CSS and absolutely linked          *
+                         * CSS is supported                               *
+                         * ************************************************/
+
+                        //XMLWorker also reads from a TextReader and not directly from a string
+                        using (var srHtml = new StringReader(example_html))
+                        {
+
+                            //Parse the HTML
+                            XMLWorkerHelper.GetInstance().ParseXHtml(writer, doc, srHtml);
+                        }
+                        
+                        doc.Close();
+                    }
+                }
+
+                //After all of the PDF "stuff" above is done and closed but **before** we
+                //close the MemoryStream, grab all of the active bytes from the stream
+                bytes = ms.ToArray();
+            }
+
+            return bytes;
+        }
+    }
+}
